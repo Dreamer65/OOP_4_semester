d3736f5037639ad539264e1d98ba09ed23a52b81
Sergey <sergeysutormen11@live.ru>
2019-02-04 22:31:57 +1100

    Добавьте файлы проекта.

---
+++ Conway's_Game_Of_Live/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conway_s_Game_Of_Live
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
